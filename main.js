bf48606a97e4e6fb6234f3fd3b7092814d725fc0
Rupali Sisilia Rozario <rozariorupali@gmail.com>
2022-09-05 14:47:43 +0600

    up date

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const navIconEl = document.querySelector('.nav__icon');
 const navCloseEl = document.querySelector('.nav__close');
-const navListEl = document.querySelector('.nav__list');
+const navList = document.querySelector('.nav__list');
 const navBgoverlayEl = document.querySelector('.nav__bgoverlay');
 
 const navOpen = () => {
-	navListEl.classList.add('show');
+	navList.classList.add('show');
 	navBgoverlayEl.classList.add('active');
-	document.body.style='visibility: visible, height: 100vh; width: 100vw; overflow:hidden;'
+	document.body.style='visibility: visible; height: 100vh; width: 100vw; overflow:hidden;';
 }
+
+const navClose = () => {
+	navList.classList.remove('show');
+	navBgoverlayEl.classList.remove('active');
+	document.body.style='visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+navCloseEl.addEventListener('click', navClose);
+navBgoverlayEl.addEventListener('click', navClose);
+
+
